0f95adbff7de65b7fd7a6b879721a75a59fb2e36
Johnson <1513858961@mil>
2019-02-15 11:30:43 -0500

    finsih pt4, started pt5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+#from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
+
 def detail(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
@@ -15,13 +17,14 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+#def vote(request, question_id):
+#   return HttpResponse("You're voting on question %s." % question_id)
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5] 
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
